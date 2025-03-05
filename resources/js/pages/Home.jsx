3ec3a52f1a3a77b4034c02620728806adaa95489
Rachid <rachidbentaleb2002@gmail.com>
2025-03-05 01:19:36 +0000

    Add SVG logos and styles for user signup and home pages

--- resources/js/pages/Home.jsx
+++ resources/js/pages/Home.jsx
@@ -1,13 +1,38 @@
 import { Link } from "@inertiajs/react";
+import './Home.css'
+import logo from '../../../public/images/logo.svg'
+import googleicone from '../../../public/images/google-icone.svg'
+import backlines from '../../../public/images/backlines.svg'
+
 
 export default function Home(){
     return (
-        <>
-            <h1>Home</h1>
-            <p>Welcome to the Home page</p>
-            <Link href="/utilisateurs/create">Creer un compte</Link>
-            <br />
-            <Link href="/login">se connecter</Link>
-        </>
+        <div className="home">
+            <img  className='backlines' src={backlines} alt="backlines" />
+            <div className="headline">
+                <h1>Connect with people. Share your world.</h1>
+                <p>A new way to connect with friends and like-minded people.</p>
+            </div>
+            <div className="signupcontainer">
+                <div className="logo">
+                    <img src={logo} alt="logo" />
+                </div>
+                <div className="titledescription">
+                    <h2>Stay connected with the people that matter.</h2>
+                    <p>Sign up to see more</p>
+                </div>
+                <div className="call-to-action">
+                    <button className="signup"><Link href="/utilisateurs/create">Sign Up</Link></button>
+                    <button><Link href="/login">Sign in</Link></button>
+                    <button className="google">
+                        <span><img src={googleicone} alt="google-icone" /></span>
+                        <p>Sign in with google</p>
+                    </button>
+                </div>
+                <div className="conditions">
+                    <p>By creating an account, you agree to <span> the Terms of use</span> <span>and Privacy Policy.</span> </p>
+                </div>
+            </div>
+        </div>
     )
 }
--- resources/js/pages/utilisateurs/Create.jsx
+++ resources/js/pages/utilisateurs/Create.jsx
@@ -1,4 +1,6 @@
 import { useForm } from "@inertiajs/react";
+import logo from '../../../../public/images/LOGO.svg';
+import "./Create.css";
 
 export default function Create() {
     const { data, setData, post, errors, processing } = useForm({
@@ -19,89 +21,93 @@ export default function Create() {
     }
 
     return (
-        <>
-            <h1>Signup</h1>
-            <p>Welcome to the Signup page</p>
-            <form onSubmit={submit} encType="multipart/form-data">
-                <label htmlFor="nom">Nom:</label>
-                <input
-                    type="text"
-                    name="nom"
-                    value={data.nom}
-                    onChange={(e) => setData('nom', e.target.value)}
-                />
-                {errors.nom && <div className="error">{errors.nom}</div>}
+        <div className="signup">
+            <div className="logo">
+                <img src={logo} alt="Logo" />
+            </div>
+            <div className="signupform">
+                <div className="signupform-header">
+                    <h2>Create Your Account</h2>
+                    <p>Create Your Account Unlock your social world and share your moments with friends and followers. Let’s get you started!</p>
+                </div>
+                <div className="inputs">
+                    <form onSubmit={submit} encType="multipart/form-data">
+                        <div className="nom-prenom">
+                            <label htmlFor="nom">Nom:</label>
+                            <input
+                                type="text"
+                                name="nom"
+                                value={data.nom}
+                                onChange={(e) => setData('nom', e.target.value)}
+                            />
+                            {errors.nom && <div className="error">{errors.nom}</div>}
 
-                <br />
+                            <label htmlFor="prenom">Prénom:</label>
+                            <input
+                                type="text"
+                                name="prenom"
+                                value={data.prenom}
+                                onChange={(e) => setData('prenom', e.target.value)}
+                            />
+                            {errors.prenom && <div className="error">{errors.prenom}</div>}
+                        </div>
 
-                <label htmlFor="prenom">Prénom:</label>
-                <input
-                    type="text"
-                    name="prenom"
-                    value={data.prenom}
-                    onChange={(e) => setData('prenom', e.target.value)}
-                />
-                {errors.prenom && <div className="error">{errors.prenom}</div>}
 
-                <br />
+                        <label htmlFor="email">Email:</label>
+                        <input
+                            type="email"
+                            name="email"
+                            value={data.email}
+                            onChange={(e) => setData('email', e.target.value)}
+                        />
+                        {errors.email && <div className="error">{errors.email}</div>}
 
-                <label htmlFor="email">Email:</label>
-                <input
-                    type="email"
-                    name="email"
-                    value={data.email}
-                    onChange={(e) => setData('email', e.target.value)}
-                />
-                {errors.email && <div className="error">{errors.email}</div>}
 
-                <br />
+                        <label htmlFor="mdp">Mot de passe:</label>
+                        <input
+                            type="password"
+                            name="mdp"
+                            value={data.mdp}
+                            onChange={(e) => setData('mdp', e.target.value)}
+                        />
+                        {errors.mdp && <div className="error">{errors.mdp}</div>}
 
-                <label htmlFor="mdp">Mot de passe:</label>
-                <input
-                    type="password"
-                    name="mdp"
-                    value={data.mdp}
-                    onChange={(e) => setData('mdp', e.target.value)}
-                />
-                {errors.mdp && <div className="error">{errors.mdp}</div>}
 
-                <br />
+                        <label htmlFor="mdp_confirmation">Confirmer mot de passe:</label>
+                        <input
+                            type="password"
+                            name="mdp_confirmation"
+                            value={data.mdp_confirmation}
+                            onChange={(e) => setData('mdp_confirmation', e.target.value)}
+                        />
+                        {errors.mdp_confirmation && <div className="error">{errors.mdp_confirmation}</div>}
 
-                <label htmlFor="mdp_confirmation">Confirmer mot de passe:</label>
-                <input
-                    type="password"
-                    name="mdp_confirmation"
-                    value={data.mdp_confirmation}
-                    onChange={(e) => setData('mdp_confirmation', e.target.value)}
-                />
-                {errors.mdp_confirmation && <div className="error">{errors.mdp_confirmation}</div>}
 
-                <br />
+                        <label htmlFor="bio">Bio:</label>
+                        <br />
+                        <textarea
+                            name="bio"
+                            rows="10"
+                            value={data.bio}
+                            onChange={(e) => setData('bio', e.target.value)}
+                        />
+                        {errors.bio && <div className="error">{errors.bio}</div>}
 
-                <label htmlFor="bio">Bio:</label>
-                <br />
-                <textarea
-                    name="bio"
-                    rows="10"
-                    value={data.bio}
-                    onChange={(e) => setData('bio', e.target.value)}
-                />
-                {errors.bio && <div className="error">{errors.bio}</div>}
 
-                <br />
+                        <label htmlFor="photodeprofile">Profile Picture:</label>
+                        <input
+                            type="file"
+                            name="photodeprofile"
+                            onChange={(e) => setData('photodeprofile', e.target.files[0])}
+                        />
+                        {errors.photodeprofile && <div className="error">{errors.photodeprofile}</div>}
 
-                <label htmlFor="photodeprofile">Profile Picture:</label>
-                <input
-                    type="file"
-                    name="photodeprofile"
-                    onChange={(e) => setData('photodeprofile', e.target.files[0])}
-                />
-                {errors.photodeprofile && <div className="error">{errors.photodeprofile}</div>}
 
-                <br />
-
-                <button type="submit" disabled={processing}>Create</button>
-            </form>
-        </>
+                        <button type="submit" disabled={processing}>Create</button>
+                    </form>
+                </div>
+            </div>
+            
+        </div>
     );
 }
